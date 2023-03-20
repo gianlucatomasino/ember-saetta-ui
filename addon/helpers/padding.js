2c9cb4435f4f076e8dd71c15a5b0c31326eb45ea
Gianluca Tomasino <email@gianluca.im>
2023-03-20 13:33:55 +0100

    Added icon to searchbox and added padding logic

--- addon/helpers/padding.js
+++ addon/helpers/padding.js
@@ -1,5 +1,12 @@
 import { helper } from '@ember/component/helper';
+import { typeOf } from '@ember/utils';
+import { isPresent } from '@ember/utils';
 
-export default helper(function padding(positional /*, named*/) {
-  return positional;
+export default helper(function padding([value]) {
+  if (typeOf(value) === 'object') {
+    if (isPresent(value['left'])) return `pl-${value['left']}`;
+    if (isPresent(value['right'])) return `pr-${value['right']}`;
+    if (isPresent(value['top'])) return `pt-${value['top']}`;
+    if (isPresent(value['bottom'])) return `pb-${value['bottom']}`;
+  }
 });
