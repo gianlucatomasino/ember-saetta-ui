d7fe8eb17f3948e1271e0fb0eb9737262dd73175
Gianluca Tomasino <email@gianluca.im>
2023-03-24 14:54:43 +0100

    Changed API for with-default

--- addon/helpers/with-default.js
+++ addon/helpers/with-default.js
@@ -1,8 +1,8 @@
 import { helper } from '@ember/component/helper';
 import { isPresent } from '@ember/utils';
 
-export default helper(function withDefault([value], { def }) {
+export default helper(function withDefault([value, defaultValue]) {
   if (isPresent(value)) return value;
 
-  return def;
+  return defaultValue;
 });
